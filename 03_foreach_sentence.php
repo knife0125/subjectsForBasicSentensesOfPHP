f7391d3ad7cce13954dc26934ad0ad01a13c3e8c
Nishide Daisuke <d.nishide@gmail.com>
2014-11-19 15:42:28 +0800

    Add subjects for basic senetenses of PHP.

---
+++ 03_foreach_sentence.php
@@ -0,0 +1,10 @@
+<?php
+    // 連想配列を作成します
+    $array = array(
+        "first" => "hogehoge",
+        "second" => "fugafuga",
+        "third" => "mogemoge"
+    );
+
+    // foreach文を使って、key, valueをそれぞれ毎回echoするような制御構文を書いて下さい
+?>
