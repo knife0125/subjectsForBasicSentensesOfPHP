30ad824301bb0c456602ce3ce05b95d08fff9825
Nishide Daisuke <d.nishide@gmail.com>
2014-11-20 13:22:58 +0800

    Adding answers for functions subject.

--- 04_functions_sentence_01.php
+++ 04_functions_sentence_01.php
@@ -1,12 +1,13 @@
 <?php
 
     // 1 + 1 をしてその結果を返す関数を定義してください
-
+    function onePlusOne() {
+        return 1 + 1;
+    }
 
     // 1 + 1 をする関数を呼んで(コールして)、その結果を$resultという変数に格納してください
-
+    $result = onePlusOne();
 
     // $resultの結果をvar_dumpで出力しておいてください
-
-
+    var_dump($result);
 ?>
